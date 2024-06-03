cff123f665266716c08fd5d8063bc0904d23f3a6
juborajMojumdar <juborajmojumdar4321@gmail.com>
2024-06-03 12:24:04 +0600

    Create Problem_No-04.c

---
+++ Problem_No-04.c
@@ -0,0 +1,23 @@
+//Develop a C program that calculates the factorial of a non-negative integer
+//'n' entered by the user
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long fact = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %d", n, fact);
+    }
+
+    return 0;
+}
